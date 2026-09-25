b5c2c50c6cb39387250bdac7a8c06e86de5bb989
TheTai <thetai9879654321@gmail.com~>
2026-09-25 16:03:42 +0700

    fix: add missing video_feed endpoint

--- worker/web_visualizer.py
+++ worker/web_visualizer.py
@@ -258,6 +258,11 @@ def index():
         
         Giám Sát Té Ngã Trực Tiếp
         """
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == "__main__":
     print("==================================================")
     print("  HE THONG DA SAN SANG!")
